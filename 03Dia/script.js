07178bb6200f0bc2a5755af46bff6b5c5fbc99a2
Keu Santos <modulodigital69@gmai.com>
2022-04-03 08:49:26 -0300

    manutenção code

--- 03Dia/script.js
+++ 03Dia/script.js
@@ -4,14 +4,58 @@ function inicia() {
 
     if (profissão == "F" || profissão == "f") {
             
-            window.location.href = "/html/fe.html"
+            window.location.href = "./html/fe/fe.html"
 
     } else if (profissão == "B" || profissão == "b") {
 
-            window.location.href = "/html/be.html"
+            window.location.href = "./html/be/be.html"
             
     } else {
-            alert("Insira a letra respectivo da profissão corretamente!")
+            alert("Insira a letra respectivo da profissão corretamente!");
             inicia();
     }
+}
+
+function fe() {
+
+       var casoDuo = prompt("Quer aprender React ou aprender Vue");
+
+       if( casoDuo.toUpperCase() == "REACT"){
+
+        window.location.href = "/html/fe/feSelectior.html"
+
+       } else {
+
+        window.location.href = "/html/fe/feSelectior.html"
+
+       }
+}
+
+function be() {
+
+        var casoDuo = prompt("Quer aprender C# ou aprender Java");
+ 
+        if( casoDuo.toUpperCase() == "C#"){
+ 
+        alert("Legal");
+        window.location.href = "/html/be/beSelectior.html"
+ 
+        } else {
+ 
+        alert("iue");
+        window.location.href = "/html/be/beSelectior.html"
+         
+        }
+ }
+
+function tecnologias() {
+        var techSelecionadas = []
+        var continuar = true;
+
+        while (continuar) {
+                var techDigitada = prompt("Digite  o que vc quer aprender!");
+                techSelecionadas.push(techDigitada);
+                continuar = confirm("Tem mais alguma tecnologia que você gostaria de aprender?")
+        }
+        console.log(techSelecionadas);
 }
